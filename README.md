f196d054abd3d6110fd04bfaa422b1b64897ddce
Saul Shanabrook <s.shanabrook@gmail.com>
2016-09-12 22:59:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# DARKWORLD
+
+## Local Development
+
+```bash
+docker run --rm -v $(pwd):/srv/jekyll -p 4000:4000 jekyll/jekyll 
+open http://localhost:4000
+
+```
---
+++ js/main.js
@@ -0,0 +1,29 @@
+function getArtists() {
+  return document.getElementById("artists").children;
+}
+
+function isArtistVisible(i) {
+  return getArtists()[i].children[1].className === "visible";
+}
+
+function setArtistVisible(i, visible) {
+  var text = visible ? "visible" : "";
+  getArtists()[i].children[1].className = text;
+}
+
+function makeArtistOnClick(i) {
+  return function () {
+    if (isArtistVisible(i)) {
+      setArtistVisible(i, false);
+      return;
+    }
+    for (var i2 = 0; i2 < getArtists().length; i2++) {
+      setArtistVisible(i2, i2 === i);
+    }
+  }
+}
+
+for (var i = 0; i < getArtists().length; i++) {
+  getArtists()[i].children[0].addEventListener("click", makeArtistOnClick(i), false);
+
+}
